8285e5be563b764b43e41fe978a0a13578434b9b
John Ramey <johnramey@gmail.com>
2014-03-20 01:41:38 -0500

    Tidying helper functions.

--- libraries/helpers.py
+++ libraries/helpers.py
@@ -1,10 +1,10 @@
 # This function lists the first n primes including n, if n is prime
-def list_first_n_primes( n ):
+def list_first_n_primes(n):
     if n < 2:
         return []
     list_of_primes = []
-    list_of_primes.append( 2 )
-    for i in range( 3, n + 1, 2 ):
+    list_of_primes.append(2)
+    for i in range(3, n + 1, 2):
         for j in list_of_primes:
             if i % j == 0:
                 break
